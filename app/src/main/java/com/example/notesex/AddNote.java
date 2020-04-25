30a226d6252de773382989f7f27d7897a112045f
Nick <nickeichenb@gmail.com>
2020-04-25 18:18:12 -0400

    Multipicture functionality

--- app/src/main/java/com/example/notesex/AddNote.java
+++ app/src/main/java/com/example/notesex/AddNote.java
@@ -95,10 +95,11 @@ public class AddNote extends AppCompatActivity {
 
         //Get current date and time
         calendar = Calendar.getInstance();
-        todaysDate = (calendar.get(Calendar.MONTH)+1) + "/" + calendar.get(Calendar.DAY_OF_MONTH)+ "/" + calendar.get(Calendar.YEAR)  ;
-        currentTime = pad(calendar.get(Calendar.HOUR)) + ":" + pad(calendar.get(Calendar.MINUTE));
+        todaysDate = (calendar.get(Calendar.MONTH)+1) + "_" + calendar.get(Calendar.DAY_OF_MONTH)+ "_" + calendar.get(Calendar.YEAR)  ;
+        currentTime = pad(calendar.get(Calendar.HOUR)) + "_" + pad(calendar.get(Calendar.MINUTE));
         Log.d("calendar", "Date and Time: " + todaysDate +" and " + currentTime);
-        timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        timeStamp = todaysDate+currentTime;
+
     }
 
     private String pad(int i) {
